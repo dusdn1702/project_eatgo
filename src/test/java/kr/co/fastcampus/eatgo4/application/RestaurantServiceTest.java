3b9e3ad74122a89fb3b8109057c2d8c7400b34ff
dusdn1702 <dusdn1702@naver.com>
2020-06-10 17:51:09 +0900

    가짜 객체 생성

--- src/test/java/kr/co/fastcampus/eatgo4/application/RestaurantServiceTest.java
+++ src/test/java/kr/co/fastcampus/eatgo4/application/RestaurantServiceTest.java
@@ -3,26 +3,50 @@ package kr.co.fastcampus.eatgo4.application;
 import kr.co.fastcampus.eatgo4.domain.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 
 public class RestaurantServiceTest {
     private RestaurantService restaurantService;
 
+    @Mock
     private RestaurantRepository restaurantRepository;
 
+    @Mock
     private MenuItemRepository menuItemRepository;
 
     @Before //모든 테스트 시행 전 시행
     public void setUp() {
-        restaurantRepository = new RestaurantRepositoryImpl();
+        MockitoAnnotations.initMocks(this);
 
-        menuItemRepository = new MenuItemRepositoryImpl();
+        mockMenuItemRepository();
+        mockRestaurantRepository();
+        restaurantService = new RestaurantService(
+                restaurantRepository, menuItemRepository);
+    }
 
-        restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+    private void mockMenuItemRepository() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+        given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
+    }
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(restaurant);
+
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
     }
 
     @Test
@@ -42,5 +66,7 @@ public class RestaurantServiceTest {
         MenuItem menuItem = restaurant.getMenuItems().get(0);
 
         assertThat(restaurant.getName(), is("Bob zip"));
+
+//        assertThat(restaurant.getName(), is("Kimchi"));
     }
 }
--- src/test/java/kr/co/fastcampus/eatgo4/interfaces/RestaurantControllerTest.java
+++ src/test/java/kr/co/fastcampus/eatgo4/interfaces/RestaurantControllerTest.java
@@ -1,19 +1,21 @@
 package kr.co.fastcampus.eatgo4.interfaces;
 
 import kr.co.fastcampus.eatgo4.application.RestaurantService;
-import kr.co.fastcampus.eatgo4.domain.MenuItemRepository;
-import kr.co.fastcampus.eatgo4.domain.MenuItemRepositoryImpl;
-import kr.co.fastcampus.eatgo4.domain.RestaurantRepository;
-import kr.co.fastcampus.eatgo4.domain.RestaurantRepositoryImpl;
+import kr.co.fastcampus.eatgo4.domain.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -25,11 +27,16 @@ public class RestaurantControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    @SpyBean(RestaurantService.class)
+    @MockBean
     private RestaurantService restaurantService;
 
     @Test
     public void list() throws Exception {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
@@ -37,21 +44,29 @@ public class RestaurantControllerTest {
                 ))
                 .andExpect(content().string(
                         containsString("\"name\":\"Bob zip\"")
-                ))
-                .andExpect(content().string(
-                        containsString("Kimchi")
                 ));
     }
 
     @Test
     public void detail() throws Exception{
+        Restaurant restaurant1 = new Restaurant(1004L, "JOCKER House", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber Food", "Seoul");
+
+        given(restaurantService.getRestaurant(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurant(2020L)).willReturn(restaurant2);
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
                         containsString("\"id\":1004")
                 ))
                 .andExpect(content().string(
-                        containsString("\"name\":\"Bob zip\"")
+                        containsString("\"name\":\"JOCKER House\"")
+                ))
+                .andExpect(content().string(
+                        containsString("Kimchi")
                 ));
 
         mvc.perform(get("/restaurants/2020"))
